63601d0a85c90bef3d9e6c740cc4b4a86ebdb732
Adrian Contreras <57216473+acontrerasgonzalez@users.noreply.github.com>
2023-12-05 10:55:31 +0100

    Update sum.test.js

--- javascript/tests/sum.test.js
+++ javascript/tests/sum.test.js
@@ -1,6 +1,7 @@
 #!/usr/bin/env node
-const sum = require("../src/sum");
+/*const sum = require("../src/sum");
 
 it("sums a couple of numbers", () => {
   expect(sum(1, 2)).toBe(3);
-});
+});*/
+console.log("holaa");
